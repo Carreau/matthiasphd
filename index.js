6e6a7474f1fd2b9d75a56be8a8f9417e85d95901
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2014-03-10 18:33:10 +0100

    commit init

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+var auth = express.basicAuth(function(user,pass) {
+      return ('user' === user && 'pass' === pass)
+            || (user === 'matthias' && pass == 'matthias')
+          ;
+});
+
+app.use('/', auth, express.static(__dirname + '/files'));
+
+app.listen();
+var port =  process.env.PORT||5000
+app.listen(port);
+console.log('listening on ', port)
